533c6a000cc668821271f189694abcc9d33f3f52
emii <eizquierd@gmail.com>
2015-01-24 18:36:58 +0100

    manual merged matt changes

--- static/js/main.js
+++ static/js/main.js
@@ -1,36 +1,35 @@
 $(document).ready(function() {
   $(document).scroll(function(){
     if ($(document).scrollTop() - $("#contact").offset().top > -200) {
-      $(".menu a").removeClass("active");
+      $("nav a").removeClass("active");
       $("#acontact").addClass("active");
     }
     else if ($(document).scrollTop() - $("#sponsors").offset().top > -200) {
-      $(".menu a").removeClass("active");
+      $("nav a").removeClass("active");
       $("#asponsors").addClass("active");
     }
     else if ($(document).scrollTop() - $("#registration").offset().top > -200) {
-      $(".menu a").removeClass("active");
+      $("nav a").removeClass("active");
       $("#aregistration").addClass("active");
     }
     else if ($(document).scrollTop() - $("#travel").offset().top > -200) {
-      $(".menu a").removeClass("active");
+      $("nav a").removeClass("active");
       $("#atravel").addClass("active");
     }
     else if ($(document).scrollTop() - $("#programme").offset().top > -200) {
-      $(".menu a").removeClass("active");
+      $("nav a").removeClass("active");
       $("#aprogramme").addClass("active");
     }
     else if ($(document).scrollTop() - $("#speakers").offset().top > -200) {
-      $(".menu a").removeClass("active");
+      $("nav a").removeClass("active");
       $("#aspeakers").addClass("active");
     }
     else if ($(document).scrollTop() - $("#about").offset().top > -200) {
-      $(".menu a").removeClass("active");
+      $("nav a").removeClass("active");
       $("#aabout").addClass("active");
     }
-    /*else if ($(document).scrollTop() - $("#home").offset().top > -200) {
-      $(".menu a").removeClass("active");
-      $("#ahome").addClass("active");
-    }*/
+    else  {
+      $("nav a").removeClass("active");
+    }
   });
 });
