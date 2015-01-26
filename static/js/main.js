12244f7a49810a0746350a7441f979f21ea29c6b
Matt Jeffryes <mjj@ebi.ac.uk>
2015-01-26 14:45:08 +0000

    Collapse menu items when screen is too small, make nav smaller on very small screens

--- static/js/main.js
+++ static/js/main.js
@@ -1,35 +1,45 @@
 $(document).ready(function() {
+	var reset_active = function() {
+		$("nav a").removeClass("active");
+		$("nav li").removeClass("active");
+	};
+	
+	var set_active = function(active) {
+		$("#a" + active).addClass("active");
+		$("#l" + active).addClass("active");
+	}
+      
   $(document).scroll(function(){
     if ($(document).scrollTop() - $("#contact").offset().top > -200) {
-      $("nav a").removeClass("active");
-      $("#acontact").addClass("active");
+      reset_active();
+      set_active('contact');
     }
     else if ($(document).scrollTop() - $("#sponsors").offset().top > -200) {
-      $("nav a").removeClass("active");
-      $("#asponsors").addClass("active");
+      reset_active();
+      set_active('sponsors');
     }
     else if ($(document).scrollTop() - $("#registration").offset().top > -200) {
-      $("nav a").removeClass("active");
-      $("#aregistration").addClass("active");
+      reset_active();
+      set_active('registration');
     }
     else if ($(document).scrollTop() - $("#travel").offset().top > -200) {
-      $("nav a").removeClass("active");
-      $("#atravel").addClass("active");
+      reset_active();
+      set_active('travel');
     }
     else if ($(document).scrollTop() - $("#programme").offset().top > -200) {
-      $("nav a").removeClass("active");
-      $("#aprogramme").addClass("active");
+      reset_active();
+      set_active('programme');
     }
     else if ($(document).scrollTop() - $("#speakers").offset().top > -200) {
-      $("nav a").removeClass("active");
-      $("#aspeakers").addClass("active");
+      reset_active();
+      set_active('speakers');
     }
     else if ($(document).scrollTop() - $("#about").offset().top > -200) {
-      $("nav a").removeClass("active");
-      $("#aabout").addClass("active");
+      reset_active();
+      set_active('about');
     }
     else  {
-      $("nav a").removeClass("active");
+      reset_active()
     }
   });
 });
