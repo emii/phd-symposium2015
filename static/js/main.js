b466c92ffd7e5323fa0dd4903e44fb18353fe9f4
emii <eizquierd@gmail.com>
2015-01-27 16:27:54 +0100

    minor enhancements

--- static/js/main.js
+++ static/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     
     $('nav ul').click(function(e){ 
         e.stopPropagation();
-    });
+      });
     
     $('nav ul li a').click(function(e){
         if ($('nav').hasClass('show')) {
@@ -28,36 +28,49 @@ $(document).ready(function() {
     }
       
   $(document).scroll(function(){
-    if ($(document).scrollTop() - $("#contact").offset().top > -200) {
+    if ($(document).scrollTop() - $("#previous").offset().top > -400) {
       reset_active();
       set_active('contact');
+      $("#down").attr("href","#header-wrapper");
+    }
+    else if ($(document).scrollTop() - $("#contact").offset().top > -200) {
+      reset_active();
+      set_active('contact');
+      $("#down").attr("href","#previous");
     }
     else if ($(document).scrollTop() - $("#sponsors").offset().top > -200) {
       reset_active();
       set_active('sponsors');
+      $("#down").attr("href","#contact");
     }
     else if ($(document).scrollTop() - $("#registration").offset().top > -200) {
       reset_active();
       set_active('registration');
+      $("#down").attr("href","#sponsors");
     }
     else if ($(document).scrollTop() - $("#travel").offset().top > -200) {
       reset_active();
       set_active('travel');
+      $("#down").attr("href","#registration");
     }
     else if ($(document).scrollTop() - $("#programme").offset().top > -200) {
       reset_active();
       set_active('programme');
+      $("#down").attr("href","#travel");
     }
     else if ($(document).scrollTop() - $("#speakers").offset().top > -200) {
       reset_active();
       set_active('speakers');
+      $("#down").attr("href","#programme");
     }
     else if ($(document).scrollTop() - $("#about").offset().top > -200) {
       reset_active();
       set_active('about');
+      $("#down").attr("href","#speakers");
     }
     else  {
       reset_active()
+      $("#down").attr("href","#about");
     }
   });
 });
